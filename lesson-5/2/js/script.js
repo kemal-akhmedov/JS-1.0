332093955a870d1a3bd617d99baf3183d98d02f2
kemal-akhmedov <37743798+kemal-akhmedov@users.noreply.github.com>
2018-04-03 23:07:07 +0300

    Add files via upload

---
+++ lesson-5/2/js/script.js
@@ -0,0 +1,41 @@
+// 2 задание	
+
+// 1 пункт
+let openStote = document.getElementById('open-btn');
+// console.log(openStote);
+
+// 2 пункт
+let nameStore = document.getElementsByClassName('name');
+// console.log(nameStore);
+let budgetStore = document.getElementsByClassName('budget');
+// console.log(budgetStore);
+let goodsStore = document.getElementsByClassName('goods');
+// console.log(goodsStore);
+let itemsStore = document.getElementsByClassName('items');
+// console.log(itemsStore);
+let employersStore = document.getElementsByClassName('employers');
+// console.log(employersStore);
+let discountStore = document.getElementsByClassName('discount');
+// console.log(discountStore);
+let isopenStore = document.getElementsByClassName('isopen');
+// console.log(isopenStore);
+
+// 3 пункт
+let goodsItem = document.querySelectorAll('.goods-item');
+// console.log(goodsItem);
+
+// 4 пункт
+btn = document.getElementsByTagName('button');
+// console.log(btn);
+
+// 5 пункт
+input1 = document.querySelector('.choose-item');
+// console.log(input1);
+input2 = document.querySelector('.time-value');
+// console.log(input2);
+input3 = document.querySelector('.count-budget-value');
+// console.log(input3);
+
+// 6 пункт
+inputAll = document.querySelectorAll('.hire-employers-item')
+// console.log(inputAll);
